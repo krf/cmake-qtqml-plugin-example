b497afcb44d3fc15fba20e6719781edc28c383b5
Kevin Funk <kfunk@kde.org>
2018-09-28 20:30:12 +0200

    Make plugin loading a bit more robust

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
 
     // Playing around with the test Qt plugin
     {
+        // Also check: https://doc.qt.io/qt-5/qtwidgets-tools-plugandpaint-app-example.html
         TestPluginInterface* testPlugin = nullptr;
 
         const QDir pluginsDir = app.applicationDirPath() + "/../plugins/";
@@ -41,11 +42,23 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 
-    qDebug() << app.applicationDirPath();
-    const QString qmlPluginsDir = app.applicationDirPath() + "/../qml";
+    QDir pluginsDir(app.applicationDirPath());
+#if defined(Q_OS_WIN)
+    if (pluginsDir.dirName().toLower() == "debug" || pluginsDir.dirName().toLower() == "release")
+        pluginsDir.cdUp();
+#elif defined(Q_OS_MAC)
+    if (pluginsDir.dirName() == "MacOS") {
+        pluginsDir.cdUp();
+        pluginsDir.cdUp();
+        pluginsDir.cdUp();
+    }
+#endif
+    pluginsDir.cdUp();
+    pluginsDir.cd("qml");
+
     // Add import search path
-    qDebug() << "Adding import path for QML plugins:" << qmlPluginsDir;
-    engine.addImportPath(qmlPluginsDir);
+    qDebug() << "Adding import path for QML plugins:" << pluginsDir.path();
+    engine.addImportPath(pluginsDir.path());
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty()) {
         return -1;
